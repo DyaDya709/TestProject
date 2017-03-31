a417f4dbc8a5becbc9d7256359e208fb0ab50597
DyaDya709 <dmitrii_r2list.ru>
2017-03-31 15:38:28 +0300

    Где картинка ?

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    @IBOutlet var imageView: UIImageView!
 
+
+    @IBAction func button(_ sender: UIButton) {
+    
+    imageView.image = UIImage(named: "CP.jpeg")
+    print("должна отображаться картинка")
+    }
 
 }
 
